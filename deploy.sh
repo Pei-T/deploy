0a695cfdb5e530223d5be06cdfbd8431ac363d11
Gong.Tang <Gong.Tang1@zeekrlife.com>
2022-11-28 15:24:05 +0800

    new commit

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.local/bin/ ~/.local/lib/ ~/.local/include
+cp bin/* ~/.local/bin/ -r
+cp include/* ~/.local/include/ -r
+cp lib/* ~/.local/include/ -r
+echo -e "\033[33mManual:\033[0m: Config env manually."
